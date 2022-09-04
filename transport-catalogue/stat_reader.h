190816993d18267dd130d2fd48e8f7842373ebcb
ArtoriasKein <105043650+ArtoriasKein@users.noreply.github.com>
2022-09-04 17:37:39 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,10 @@
-// место для вашего кода
+#pragma once
+#include <iostream>
+#include "transport_catalogue.h"
+
+namespace transport_catalogue {
+    namespace output {
+        std::ostream& operator<<(std::ostream& out, transport_catalogue::TransportCatalogue::Statistics statistics);
+        void ParseOutput(std::istream& input, transport_catalogue::TransportCatalogue catalogue);
+    }
+}
