92947bb30ed07cb1ac290deebd649b6ac60859f1
ArtoriasKein <105043650+ArtoriasKein@users.noreply.github.com>
2022-09-18 22:20:47 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -1,54 +1,36 @@
 #pragma once
-#include "geo.h"
-#include <string>
-#include <vector>
+#include "domain.h"
 #include <deque>
 #include <unordered_map>
 #include <set>
-#include <unordered_set>
+#include <optional>
 
 namespace transport_catalogue {
 
     class TransportCatalogue {
     public:
-        struct Stop {
-            ::std::string name;
-            Coordinates coordinates;
-        };
-
-        struct Bus {
-            ::std::string name;
-            bool is_rounded;
-            ::std::vector<const Stop*> route;
-            double distance_real;
-            double distance_ideal;
-        };
-
         void AddStop(const ::std::string& name, double latitude, double longitude);
         void AddBus(const ::std::string& name, const ::std::vector<::std::string>& stops, bool rounded);
         void AddStopDistances(const ::std::string& stop_name, const ::std::vector<::std::pair<::std::string, int>>& stops_and_distances);
-        struct Statistics {
-            bool found;
-            int stops_count;
-            int unique_stops_count;
-            double distance;
-            double curvature;
-        };
-        ::std::pair<bool, ::std::vector<::std::string>> BusesOnStop(const ::std::string& stop_name);
-        Statistics GetBusInfo(const ::std::string& bus);
+        const ::std::optional<::std::set<std::string_view>> BusesOnStop(const ::std::string_view& stop_name) const;
+        const ::std::optional<domain::Statistics> GetBusInfo(const ::std::string_view& bus) const;
+        const ::std::set<::std::string_view> GetAllBusesNames() const;
+        const ::std::vector<::std::string_view> GetBusRoute(const ::std::string_view& bus_name) const;
+        const std::vector<geo::Coordinates> GetAllStopCoordinates() const;
+        const geo::Coordinates GetStopCoordinates(const std::string_view& stop_name) const;
     private:
-        ::std::deque<Bus> buses_;
-        ::std::deque<Stop> stops_;
-        ::std::unordered_map<::std::string_view, const Stop*> stopname_to_stop_;
-        ::std::unordered_map<const Stop*, ::std::set<std::string_view>> stopname_to_busname_;
-        ::std::unordered_map<::std::string_view, const Bus*> busname_to_bus_;
+        ::std::deque<domain::Bus> buses_;
+        ::std::deque<domain::Stop> stops_;
+        ::std::unordered_map<::std::string_view, const domain::Stop*> stopname_to_stop_;
+        ::std::unordered_map<const domain::Stop*, ::std::set<std::string_view>> stopname_to_busname_;
+        ::std::unordered_map<::std::string_view, const domain::Bus*> busname_to_bus_;
         struct StopsHasher {
-            size_t operator()(::std::pair<const Stop*, const Stop*> stops) const {
+            size_t operator()(::std::pair<const domain::Stop*, const domain::Stop*> stops) const {
                 ::std::hash<const void*> ptr_hasher;
                 size_t result = ptr_hasher(stops.first) + ptr_hasher(stops.second) * 37;
                 return result;
             }
         };
-        ::std::unordered_map<::std::pair<const Stop*, const Stop*>, int, StopsHasher> stops_to_distance;
+        ::std::unordered_map<::std::pair<const domain::Stop*, const domain::Stop*>, int, StopsHasher> stops_to_distance;
     };
 }
