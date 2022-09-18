51aa3411932c73c271254d94f31bea1408abf2c2
ArtoriasKein <105043650+ArtoriasKein@users.noreply.github.com>
2022-09-18 22:19:24 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,8 +1,5 @@
-#include <iostream>
-#include "input_reader.h"
-#include "stat_reader.h"
+#include "json_reader.h"
 
 int main() {
-    transport_catalogue::TransportCatalogue transport_catalogue = transport_catalogue::input::ParseInput(std::cin);
-    transport_catalogue::output::ParseOutput(::std::cin, transport_catalogue);
+    ParseInput(std::cin);
 }
