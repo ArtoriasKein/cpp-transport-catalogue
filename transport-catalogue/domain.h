ae86486b4bc35b50b4c320a85b250ca8618a2fa4
ArtoriasKein <105043650+ArtoriasKein@users.noreply.github.com>
2022-09-18 22:17:27 +0300

    Create domain.h

---
+++ transport-catalogue/domain.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+#include <vector>
+#include "geo.h"
+
+namespace domain {
+
+    struct Stop {
+        std::string name;
+        geo::Coordinates coordinates;
+    };
+
+    struct Bus {
+        std::string name;
+        bool is_rounded;
+        std::vector<const Stop*> route;
+        double distance_real;
+        double distance_ideal;
+    };
+
+    struct Statistics {
+        bool found;
+        bool is_rounded;
+        size_t stops_count;
+        size_t unique_stops_count;
+        double distance;
+        double curvature;
+    };
+} // namespace domain
