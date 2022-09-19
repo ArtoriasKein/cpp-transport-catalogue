675374242fc0f0eec2280d41ff14713b3956de2a
ArtoriasKein <105043650+ArtoriasKein@users.noreply.github.com>
2022-09-19 13:51:12 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -4,5 +4,39 @@
 #include "json.h"
 #include "request_handler.h"
 
-std::string Print(const json::Node& node);
-void ParseInput(std::istream& input);
+
+class JsonReader {
+public:
+    struct StopInput {
+        std::string name;
+        double latitude;
+        double longitude;
+    };
+
+    struct StopDistancesInput {
+        std::string name;
+        std::vector<std::pair<std::string, int>> stop_to_distance;
+    };
+
+    struct BusInput {
+        std::string name;
+        std::vector<std::string> route;
+        bool is_rounded;
+    };
+
+	JsonReader();
+	std::string Print(const json::Node& node);
+	void ParseInput(std::istream& input);
+    BusInput ParseBusInput(const json::Node& node);
+    StopInput ParseStopInput(const json::Node& node);
+    StopDistancesInput ParseStopWithDistanceInput(const json::Node& array);
+    void ParseBaseRequests(const json::Node& array);
+    void ParseRenderSettings(const json::Node& node);
+private:
+	transport_catalogue::TransportCatalogue catalogue_;
+	renderer::MapRenderer map_renderer_;
+	request_handler::RequestHandler handler;
+};
+
+
+
